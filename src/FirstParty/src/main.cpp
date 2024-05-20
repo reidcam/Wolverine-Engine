d4d5fc8ac51412c67ada25a0bb49a72856c45e1c
TheSevenSages <Jacobrobinson4321@gmail.com>
2024-05-20 11:13:24 -0400

    Created XCode Project

--- src/FirstParty/src/main.cpp
+++ src/FirstParty/src/main.cpp
@@ -3,10 +3,18 @@
 #include "SDL_image.h"
 
 int main(int argc, char* argv[]) {
-	SDL_Window* window = SDL_CreateWindow("test", 100, 100, 512, 512, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // set renderer draw color
-	SDL_RenderClear(renderer); // clear the renderer with the render clear color
-
+    IMG_Init(IMG_INIT_PNG);
+    SDL_Window* window = SDL_CreateWindow("test", 0, 0, 512, 512, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC + SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+        std::cerr << "Failed to create renderer : " << SDL_GetError() << std::endl;
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // set renderer draw color
+    while (true)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) { } // Flush event queue
+        SDL_RenderClear(renderer); // clear the renderer with the render clear color
+        SDL_RenderPresent(renderer);
+    }
 	return 0;
 }
