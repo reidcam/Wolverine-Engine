26cbfa507d15c42af29e8ef1252831c96fe2d573
niemway <niemway@umich.edu>
2024-07-15 14:44:19 -0400

    super basic spriterenderer wrapper
    still needs integration in main game loop (should be processed by renderer as a drawImageEx request

---
+++ src/FirstParty/src/SpriteRenderer.cpp
@@ -0,0 +1,22 @@
+//
+//	SpriteRenderer.cpp
+//	wolverine_engine
+//
+//	Created by Nicholas Way on 7/10/24.
+//	Defines the Sprite Renderer component
+
+#include "SpriteRenderer.h"
+
+/*
+* Lifecycle function for initializing a sprite renderer
+*/
+void SpriteRenderer::OnStart()
+{
+}
+
+/*
+* Lifecycle function for destroying the sprite renderer
+*/
+void SpriteRenderer::OnDestroy()
+{
+}
